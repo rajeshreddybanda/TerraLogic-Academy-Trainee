712cd1a343b54bca1318e37dc0602357866406e1
unknown <rajeshreddybanda@gmail.com>
2025-10-19 09:15:07 +0530

    commit

---
+++ FrontEnd/sample-react/src/BasicFile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const BasicFile = () => {
+  return (
+    <div>BasicFile1
+         <h2>welcome</h2>
+    </div>
+   
+  )
+}
+
---
+++ FrontEnd/sample-react/src/components/TestComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BasicFile } from '../BasicFile'
+
+const TestComponent = () => {
+  return (
+    <>
+    <div>TestComponent</div>
+    <BasicFile />
+    </>
+  )
+}
+
+export default TestComponent
